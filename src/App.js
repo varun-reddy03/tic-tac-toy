efe76b19a25768e9683ae9165a9b60d4ca8dbb63
varun-reddy03 <varun-reddy03>
2024-07-09 20:18:44 +0530

    Your commit message

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Board from './Board';
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1>Tic Tac Toe</h1>
+      <Board />
+    </div>
+  );
+};
+
+export default App;
+
---
+++ src/Square.js
@@ -0,0 +1,14 @@
+// Square.js
+import React from 'react';
+import './Square.css';
+
+function Square(props) {
+  return (
+    <button className="square" onClick={props.onClick}>
+      {props.value}
+    </button>
+  );
+}
+
+export default Square;
+
